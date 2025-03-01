3487b33f3ea0501c7de44f5823c9d7336cc0291e
Rubaid07 <rubaidindu@gmail.com>
2025-03-01 14:50:37 +0600

    added main section & active log

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ const today = new Date().toDateString().split(' ');
 date.innerHTML = `
     ${today[0]} , <br> <strong> ${today.slice(1).join(' ')} </strong>
 `
+
+
+// activity log
+const completeBtn = document.querySelectorAll('.complete-btn');
+const cardHead = document.querySelectorAll('.card-heading')
+for(let i = 0; i < completeBtn.length; i++){
+    const btn = completeBtn[i];
+    btn.addEventListener('click', function(event){
+        event.target.setAttribute('disabled', 'true');
+        event.target.style.backgroundColor ='#ced6ff';
+        alert('Board updated successfully');
+        
+    })
+}
+
